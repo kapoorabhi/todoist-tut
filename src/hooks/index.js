75a2ec47713f93df87dfaf6db90be5b283aeb25d
kapoorabhi <kapoor.abhi@outlook.com>
2020-05-26 11:15:46 -0400

    Setup basic structure

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,26 @@
 import { useState, useEffect } from "react";
+import moment from "moment"
 
 import { firebase } from "../firebase";
+import { collatedTasksExist } from "../helpers";
 
 export const useTasks = (selectedProject) => {
   const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection("tasks")
+      .where("userId", "==", "fErmuThEDulTaB");
+
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where("projectId", "==", selectedProject))
+        : selectedProject === "TODAY"
+        ? (unsubscribe = unsubscribe.where("date" "==", moment().format("DD/MM/YYYY")
+        ))
+        : selectedProject === "INBOX" || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where("date", "==", ""))
+        : unsubscribe;
+  }, [selectedProject]);
 };
